f14bbd2ddb833466161c79aa7519ad637bbda131
Afif Hidayat <fathurizkymaulana80.com>
2026-09-14 12:07:29 +0700

    feat: buat landing page awal umkm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
+  promoButton.textContent = "Promo: Beli 2 gratis Mba-Mba Kasir!";
   console.log("Promo Kopi Paten Kulo berhasil ditampilkan.");
 });
